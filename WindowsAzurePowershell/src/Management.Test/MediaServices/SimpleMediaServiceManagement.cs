5ca243a59e8c3eecae0cf1e26f39e164de9bfd60
George Trifonov <gtrifonov@gmail.com>
2013-07-26 09:37:21 -0700

    MediaServicesManagement skeleton

---
+++ WindowsAzurePowershell/src/Management.Test/MediaServices/SimpleMediaServiceManagement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Management.Test.Utilities.Common;
+using Microsoft.WindowsAzure.Management.Utilities.MediaService.Services;
+using Microsoft.WindowsAzure.Management.Utilities.MediaService.Services.MediaServicesEntities;
+
+namespace Microsoft.WindowsAzure.Management.Test.Utilities.MediaServices
+{
+    public class SimpleMediaServiceManagement : IMediaServiceManagement
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the thunk wrappers will
+        /// throw an exception if the thunk is not implemented.  This is useful
+        /// when debugging a test.
+        /// </summary>
+        public bool ThrowsIfNotImplemented { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleMediaServiceManagement"/> class.
+        /// </summary>
+        public SimpleMediaServiceManagement()
+        {
+            ThrowsIfNotImplemented = true;
+        }
+
+        public Func<SimpleServiceManagementAsyncResult, MediaServiceAccounts> GetMediaServicesThunk { get; set; }
+
+        public IAsyncResult BeginGetMediaServices(string subscriptionName, AsyncCallback callback, object state)
+        {
+            SimpleServiceManagementAsyncResult result = new SimpleServiceManagementAsyncResult();
+            result.Values["subscriptionName"] = subscriptionName;
+            result.Values["callback"] = callback;
+            result.Values["state"] = state;
+            return result;
+        }
+
+        public MediaServiceAccounts EndGetMediaServices(IAsyncResult asyncResult)
+        {
+            if (GetMediaServicesThunk != null)
+            {
+                SimpleServiceManagementAsyncResult result = asyncResult as SimpleServiceManagementAsyncResult;
+                Assert.IsNotNull(result, "asyncResult was not SimpleServiceManagementAsyncResult!");
+
+                return GetMediaServicesThunk(result);
+            }
+            else if (ThrowsIfNotImplemented)
+            {
+                throw new NotImplementedException("GetSitesThunk is not implemented!");
+            }
+
+            return default(MediaServiceAccounts);
+        }
+    }
+}
---
+++ WindowsAzurePowershell/src/Management.Utilities/MediaService/Services/MediaServicesEntities/IMediaServiceAccount.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using Microsoft.WindowsAzure.Management.Utilities.Websites;
+using Microsoft.WindowsAzure.Management.Utilities.Websites.Services.WebEntities;
+
+namespace Microsoft.WindowsAzure.Management.Utilities.MediaService.Services.MediaServicesEntities
+{
+    public interface IMediaServiceAccount
+    {
+        string Name { get; set; }
+        Guid AccountId { get; set; }
+        Uri ParentLink { get; set; }
+        Uri SelfLink { get; set; }
+        string State { get; set; }
+         string Type { get; set; }
+
+    }
+
+    [DataContract(Namespace = UriElements.ServiceNamespace, Name = "ServiceResource")]
+    public class MediaServiceAccount : IMediaServiceAccount
+    {
+        [DataMember(EmitDefaultValue = false, Order = 5)]
+        public Guid AccountId { get; set; }
+
+        [DataMember(EmitDefaultValue = false, Order = 0)]
+        public string Name { get; set; }
+
+        [DataMember(EmitDefaultValue = false, Order = 4)]
+        public Uri ParentLink { get; set; }
+
+        [DataMember(EmitDefaultValue = false, Order = 3)]
+        public Uri SelfLink { get; set; }
+
+        [DataMember(EmitDefaultValue = false, Order = 2)]
+        public string State { get; set; }
+
+        [DataMember(EmitDefaultValue = false, Order = 1)]
+        public string Type { get; set; }
+    }
+
+     /// <summary>
+    /// Collection of sites
+    /// </summary>
+    [CollectionDataContract(Namespace = UriElements.ServiceNamespace,Name = "ServiceResources", ItemName = "ServiceResource")]
+    public class MediaServiceAccounts : List<MediaServiceAccount>
+    {
+
+        /// <summary>
+        /// Empty collection
+        /// </summary>
+        public MediaServiceAccounts() { }
+
+        /// <summary>
+        /// Initialize collection
+        /// </summary>
+        /// <param name="mediaAccounts"></param>
+        public MediaServiceAccounts(List<MediaServiceAccount> mediaAccounts) : base(mediaAccounts) { }
+    }
+
+    /// <summary>
+    /// Paged collection of sites
+    /// </summary>
+    [DataContract(Namespace = UriElements.ServiceNamespace)]
+    public class PagedSites : PagedSet<MediaServiceAccount>
+    {
+    }
+
+
+   
+}
